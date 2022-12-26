2924b4bf59e6013f053b32641f256d56f4fd70b0
Mouhamed <mouhamedezzine534@gmail.com>
2022-12-26 18:16:30 +0100

    added web page

--- js/display.js
+++ js/display.js
@@ -1,27 +1,23 @@
-$(document).ready(function(){
-    $.getJSON("restaurants.json", function(data){
+function parseJsonLine(line){
         let htmlStructure= ''
 
-        $.each(data, function(key, value){
-            switch(key){
-                case "address":
-                    htmlStructure+= "<br><br>Address:<br>Building:"+ value.building + "<br>Coord: "+ value.coord + "<br>Street: "+ value.street+ "<br>Zipcode: "+ value.zipcode
-                    break
-
-                case "grades":
-                    htmlStructure+= "<br>Grades: "
-                    for(let i= 0; i< value.length; i++){
-                        htmlStructure+= "<br>Grade: Date: "+ value[i].date.$date+ ", Grade: "+ value[i].grade+ ", Score: " + value[i].score
-                    }
-                    break
-
-                default:
-                    htmlStructure+= "<br>"+ key + ": "+ value
-                    break
-            }
-            //htmlStructure+= 'Address: building: '+ value +"<br>"
-            console.log(key +": "+ value)
+        JSON.parse(line, function(key, value){
+            htmlStructure+= key+": "+value+"<br>"
         })
-        document.write(htmlStructure)
+        htmlStructure+= "<br><br>"
+        document.getElementById("output").innerHTML+= htmlStructure
+}
+
+
+(function readFile(){
+    jQuery.get('restaurants.txt', function(txt){
+        let lines= txt.split("\n")
+        for(let i= 0; i<5; i++){
+            line= lines[i]
+            if(line!= ""){
+                console.log(line)
+                parseJsonLine(line)
+            }
+        }
     })
-})
+}).call()
